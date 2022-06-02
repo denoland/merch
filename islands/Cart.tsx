362746360e2772804633d21d00f98564306526f7
Luca Casonato <hello@lcas.dev>
2022-06-02 23:26:40 +0200

    better twind + cart close button

--- islands/Cart.tsx
+++ islands/Cart.tsx
@@ -1,7 +1,8 @@
 /** @jsx h */
 /** @jsxFrag Fragment */
 import { Fragment, h, IS_BROWSER, useRef } from "$fresh/runtime.ts";
-import { tw } from "$twind";
+import { apply, tw } from "$twind";
+import { animation, css } from "$twind/css";
 
 // Lazy load a <dialog> polyfill.
 // @ts-expect-error HTMLDialogElement is not just a type!
@@ -18,6 +19,22 @@ declare global {
   }
 }
 
+const slideRight = animation("0.4s ease normal", {
+  from: { transform: "translateX(100%)" },
+  to: { transform: "translateX(0)" },
+});
+
+const slideBottom = animation("0.4s ease normal", {
+  from: { transform: "translateY(100%)" },
+  to: { transform: "translateY(0)" },
+});
+
+const backdrop = css({
+  "&::backdrop": {
+    background: "rgba(0, 0, 0, 0.5)",
+  },
+});
+
 export default function Cart() {
   const ref = useRef<HTMLDialogElement | null>(null);
 
@@ -29,57 +46,54 @@ export default function Cart() {
 
   return (
     <div>
-      <style
-        dangerouslySetInnerHTML={{
-          __html: `dialog[open] {
-  animation: show-bottom 0.4s ease normal;
-}
-
-@media (min-width: 640px) {
-  dialog[open] {
-    animation: show-right 0.4s ease normal;
-  }
-}
-
-@keyframes show-bottom {
-  from {
-    transform: translateY(100%);
-  }
-  to {
-    transform: translateY(0%);
-  }
-}
-
-
-@keyframes show-right {
-  from {
-    transform: translateX(100%);
-  }
-  to {
-    transform: translateX(0%);
-  }
-}
-`,
-        }}
-      >
-      </style>
+      <button onClick={() => ref.current!.showModal()}>Cart</button>
       <dialog
         ref={ref}
         class={tw
-          `p-0 m-0 pt-[50%] sm:pt-0 sm:ml-auto bg-transparent max-w-full sm:max-w-lg w-full h-full max-h-full`}
+          `bg-transparent p-0 m-0 pt-[50%] sm:pt-0 sm:ml-auto max-w-full sm:max-w-lg w-full max-h-full h-full ${slideBottom} sm:${slideRight} ${backdrop}`}
         onClick={onDialogClick}
       >
-        <div
-          class={tw
-            `py-8 px-6 h-full bg-white rounded-t-2xl sm:rounded-tr-none sm:rounded-l-2xl flex flex-col justify-between`}
-        >
-          <h1 class={tw`text-4xl font-bold m-0`}>Cart</h1>
-          <button class={tw`p-2 border border-gray-500 rounded-xl bg-gray-100`}>
-            Checkout
-          </button>
-        </div>
+        <CartInner />
       </dialog>
-      <button onClick={() => ref.current!.showModal()}>Cart</button>
+    </div>
+  );
+}
+
+function CartInner() {
+  const corners = apply`rounded(tl-2xl tr-2xl sm:(tr-none bl-2xl))`;
+  const card = tw
+    `py-8 px-6 h-full bg-white ${corners} flex flex-col justify-between`;
+
+  return (
+    <div class={card}>
+      <div class={tw`flex justify-between`}>
+        <h1 class={tw`text-4xl font-bold m-0`}>Cart</h1>
+        <button
+          class={tw`py-1`}
+          onClick={(e) => {
+            (e.target as HTMLButtonElement).closest("dialog")!.close();
+          }}
+        >
+          <svg
+            class={tw`w-6 h-6 fill-current text-gray-600`}
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+          </svg>
+        </button>
+      </div>
+
+      <div class={tw`flex-grow-1 my-4`}>
+        <p class={tw`text-gray-700`}>There are no items in the cart.</p>
+      </div>
+      <button
+        class={tw
+          `p-2 border border-gray-200 rounded-xl bg-gray-50 text-gray-500 cursor-default`}
+        disabled
+      >
+        Checkout
+      </button>
     </div>
   );
 }
