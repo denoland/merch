bf536fb0638994ea449d78b23a973de2ab6f031f
Aaron O'Mullan <aaron.omullan@gmail.com>
2022-06-09 18:58:58 +0200

    fmt

--- islands/Cart.tsx
+++ islands/Cart.tsx
@@ -48,13 +48,32 @@ export default function Cart() {
 
   return (
     <div>
-      <button onClick={() => ref.current!.showModal()} class={tw`block relative`}>
+      <button
+        onClick={() => ref.current!.showModal()}
+        class={tw`block relative`}
+      >
         {/* <span>Cart ({data?.lines.edges.length})</span> */}
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-          <path strokeLinecap="round" strokeLinejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+          />
         </svg>
-        <div class={tw`block absolute top-0 right-0 h-4 w-4 transform -translate-y-1/2 translate-x-1/2 rounded-full bg-red-500 flex`}>
-          <span class={tw`place-self-center flex-1 text-white text-[0.6rem]`}>{data?.lines.edges.length}</span>
+        <div
+          class={tw
+            `block absolute top-0 right-0 h-4 w-4 transform -translate-y-1/2 translate-x-1/2 rounded-full bg-red-500 flex`}
+        >
+          <span class={tw`place-self-center flex-1 text-white text-[0.6rem]`}>
+            {data?.lines.edges.length}
+          </span>
         </div>
       </button>
       <dialog
