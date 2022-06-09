046c12631d2a7edf5ce0cded96135415d71ba0b3
Aaron O'Mullan <aaron.omullan@gmail.com>
2022-06-09 18:58:53 +0200

    product grid

--- routes/index.tsx
+++ routes/index.tsx
@@ -35,29 +35,41 @@ export default function Home({ data }: PageProps) {
   return (
     <div>
       <NavBar />
-      <ul class={tw`text-xl px-4 sm:px-8 py-6`}>
-        {data.products.edges.map(({ node }) => (
-          <li
-            key={node.id}
-            class={tw`flex gap-4 items-center border border-gray-100`}
-          >
-            {node.featuredImage
-              ? (
+      <div
+        class={tw
+          `max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8`}
+      >
+        <h2 class={tw`sr-only`}>Products</h2>
+
+        <div
+          class={tw
+            `grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8`}
+        >
+          {data.products.edges.map(({ node }) => (
+            <a
+              key={node.id}
+              href={`/products/${node.handle}`}
+              class={tw`group`}
+            >
+              <div
+                class={tw
+                  `w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8`}
+              >
                 <img
                   src={node.featuredImage.url}
                   alt={node.featuredImage.altText}
                   width={node.featuredImage.width}
                   height={node.featuredImage.height}
-                  class={tw`w-16`}
+                  class={tw
+                    `w-full h-full object-center object-cover group-hover:opacity-75`}
                 />
-              )
-              : <div class={tw`w-16 bg-gray-100`}></div>}
-            <a href={`/products/${node.handle}`} class={tw`hover:underline`}>
-              {node.title}
+              </div>
+              <h3 class={tw`mt-4 text-sm text-gray-700`}>{node.title}</h3>
+              <p class={tw`mt-1 text-lg font-medium text-gray-900`}>$100</p>
             </a>
-          </li>
-        ))}
-      </ul>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
