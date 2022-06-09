e2c84b8a594f02783b7be56a3fcb6545490a4717
Aaron O'Mullan <aaron.omullan@gmail.com>
2022-06-09 17:03:49 +0200

    align navbar cart right

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,16 +7,14 @@ export function NavBar() {
   return (
     <nav
       class={tw
-        `h-18 px-4 sm:px-8 border-gray-200 sm:border-transparent border-b flex items-center gap-4`}
+        `h-18 px-4 sm:px-8 border-gray-200 sm:border-transparent border-b flex items-center justify-between gap-4`}
     >
-      <a href="/">
+      <a href="/" class={tw`flex flex-none gap-x-2`}>
         <img
           src="/logo.svg"
           alt="Deno Logo"
           class={tw`h-7 w-7`}
         />
-      </a>
-      <a href="/">
         <h1 class={tw`text-xl`}>
           Deno <span class={tw`font-light`}>Shop</span>
         </h1>
