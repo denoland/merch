0cb44e5c3dd44d645235fea95f8cba337d5bb323
Aaron O'Mullan <aaron.omullan@gmail.com>
2022-06-10 10:48:20 +0200

    cleanup

--- routes/products/[product].tsx
+++ routes/products/[product].tsx
@@ -49,68 +49,63 @@ export default function Home({ data }: PageProps) {
 
 function ProductDetails({ data }: Record<string, any>) {
   return (
-    <div class={tw`bg-white`}>
+    <div
+      class={tw
+        `max-w-2xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8`}
+    >
+      {/* Product details */}
+      <div class={tw`lg:max-w-lg`}>
+        <h1
+          class={tw
+            `text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl`}
+        >
+          {data.product.title}
+        </h1>
+
+        <section aria-labelledby="information-heading" class={tw`mt-4`}>
+          <h2 id="information-heading" class={tw`sr-only`}>
+            Product information
+          </h2>
+
+          <div class={tw`flex items-center`}>
+            <p class={tw`text-lg text-gray-900 sm:text-xl`}>$100</p>
+          </div>
+
+          <div class={tw`mt-4 space-y-6`}>
+            <p class={tw`text-base text-gray-500`}>
+              {data.product.description}
+            </p>
+          </div>
+        </section>
+      </div>
+
+      {/* Product image */}
+      <div
+        class={tw`mt-10 lg:mt-0 lg:col-start-2 lg:row-span-2 lg:self-center`}
+      >
+        <div class={tw`rounded-lg overflow-hidden`}>
+          <img
+            src={data.product.featuredImage.url}
+            alt={data.product.featuredImage.altText}
+            width={data.product.featuredImage.width}
+            height={data.product.featuredImage.height}
+            class={tw`w-full h-full object-center object-cover`}
+          />
+        </div>
+      </div>
+
+      {/* Product form */}
       <div
         class={tw
-          `max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8`}
+          `mt-10 lg:max-w-lg lg:col-start-1 lg:row-start-2 lg:self-start`}
       >
-        {/* Product details */}
-        <div class={tw`lg:max-w-lg`}>
-          <div class={tw`mt-4`}>
-            <h1
-              class={tw
-                `text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl`}
-            >
-              {data.product.title}
-            </h1>
-          </div>
-
-          <section aria-labelledby="information-heading" class={tw`mt-4`}>
-            <h2 id="information-heading" class={tw`sr-only`}>
-              Product information
-            </h2>
-
-            <div class={tw`flex items-center`}>
-              <p class={tw`text-lg text-gray-900 sm:text-xl`}>$100</p>
+        <section aria-labelledby="options-heading">
+          <form>
+            <div class={tw`mt-10`}>
+              <AddToCart id={data.product.variants.edges[0].node.id} />
             </div>
-
-            <div class={tw`mt-4 space-y-6`}>
-              <p class={tw`text-base text-gray-500`}>
-                {data.product.description}
-              </p>
-            </div>
-          </section>
-        </div>
-
-        {/* Product image */}
-        <div
-          class={tw`mt-10 lg:mt-0 lg:col-start-2 lg:row-span-2 lg:self-center`}
-        >
-          <div class={tw`rounded-lg overflow-hidden`}>
-            <img
-              src={data.product.featuredImage.url}
-              alt={data.product.featuredImage.altText}
-              width={data.product.featuredImage.width}
-              height={data.product.featuredImage.height}
-              // class={tw`mt-4 w-32`}
-              class={tw`w-full h-full object-center object-cover`}
-            />
-          </div>
-        </div>
-
-        {/* Product form */}
-        <div
-          class={tw
-            `mt-10 lg:max-w-lg lg:col-start-1 lg:row-start-2 lg:self-start`}
-        >
-          <section aria-labelledby="options-heading">
-            <form>
-              <div class={tw`mt-10`}>
-                <AddToCart id={data.product.variants.edges[0].node.id} />
-              </div>
-            </form>
-          </section>
-        </div>
+          </form>
+        </section>
       </div>
     </div>
   );
