baca3f7c7acc638e34e6dc4f2be6c03537292d6b
cjihrig <cjihrig@gmail.com>
2022-06-29 16:28:57 -0400

    redirect to hosted checkout page

--- islands/Cart.tsx
+++ islands/Cart.tsx
@@ -112,6 +112,7 @@ function CartInner(props: { cart: CartData | undefined }) {
   const checkout = (e: Event) => {
     e.preventDefault();
     console.log("checkout");
+    location.href = data.checkoutUrl;
   };
 
   return (
